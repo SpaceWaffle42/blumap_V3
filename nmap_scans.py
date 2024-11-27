3af588d5eaed055a90d21f2a74220a0f79976d45
SpaceWaffle42 <86594101+SpaceWaffle42@users.noreply.github.com>
2024-11-27 22:17:47 +0000

    Scans to DB
    added functionality to save scans to DB, with some filtering to reduce noise (empty addresses) entering the DB.

--- nmap_scans.py
+++ nmap_scans.py
@@ -1,34 +1,107 @@
 from nmap3 import Nmap
 import datetime
-import os
-import pathlib
-
+from database import Database as db
+import time
 
 class scan():
     nm = Nmap()
-    multi_scan = True
     def scan(self, ip, notation, top, auto):
-        scan_arg = '-O'
+        multi_scan = True
+        notation_scan = False
+
+        if not notation:
+            notation = 24
+
+        if not ip:
+            ip = "127.0.0.1"
+
+        scan_arg = ['-O']
         if notation != "0": #If notation is not == 0
             octets = ip.split(".") #Split IP into parts
             octets[-1] = "0" #Replaces the last item with a 0
             ip = ".".join(octets) #Rejoin the octets
             ip = ip + f"/{notation}"#append the notation to the IP
+            notation_scan = True
 
-        while scan.multi_scan == True:
+            print("ip ",ip)
+        while multi_scan == True:
             date = datetime.datetime.now().strftime("%Y-%m-%d")
             time_now = datetime.datetime.now().strftime("%H:%M")
             now = f"{date} {time_now}"
+
             if top:
                 sub_scan = scan.nm.scan_top_ports(target=ip, args=scan_arg)
-            else:
+
+            elif not top or notation_scan == True:
                 sub_scan = scan.nm.nmap_subnet_scan(target=ip, args=scan_arg)
-            scan.multi_scan = auto
 
-        for host, info in sub_scan.items():
-            print("\n HOST!!!!!",host,"\n INFO!!!!!",info,"\n ITEMS!!!!",sub_scan.items())
-        print("scan os")
+            multi_scan = auto
 
+            for host_, details in sub_scan.items():
+                if type(details) == dict:
+                    try:
+                            mac = details.get("macaddress", {})["addr"]
+                    except:
+                        mac = None
+                    try:
+                        vendor = details.get("macaddress", {})["vendor"]
+                    except:
+                        vendor = None
+
+                    try:
+                        cpe = None
+                        os_name = None
+                        host_name = None
+                        accuracy = None
+
+                        p_open = []
+                        p_closed = []
+                        p_filtered = []
+
+                        if len(details.get("ports", [])) > 0:
+                            for port_info in details["ports"]:
+                                if len(details["osmatch"]) >= 0:
+                                    try:
+                                        cpe = details["osmatch"][0]["cpe"]
+                                    except:
+                                        cpe = None
+
+                                    try:
+                                        os_name = details["osmatch"][0]["name"]
+                                    except:
+                                        os_name = None
+
+                                    try:
+                                        accuracy = details["osmatch"][0]["accuracy"]
+                                    except:
+                                        accuracy = None
+
+                                    try:
+                                        host_name = details["hostname"][0]["name"]
+                                    except:
+                                        host_name = None
+
+                                if "closed" in port_info['state']:
+                                    p_closed.append(int(port_info['portid']))
+
+                                if "open" in port_info['state']:
+                                    p_open.append(int(port_info['portid']))
+
+                                if "filtered" in port_info['state']:
+                                    p_filtered.append(int(port_info['portid']))
+
+                                    
+                    except Exception as e:
+                        print(f"nmap_scans Error: {e}")
+                if host_name == None and mac == None and len(p_closed) == 0 and len(p_open) == 0 and len(p_filtered) == 0:
+                    pass
+                else:
+                    db.save(host_, host_name, mac, vendor, os_name, accuracy, cpe, p_open, p_closed, p_filtered, now)
+                    print(f"Scan performed at {now}")
+                time.sleep(int(sleeper))
+                multi_scan = False
+
+                            # print(key, value,'\n')
         if dev == True:
             print(
             f"""
@@ -46,10 +119,10 @@ if __name__ == "__main__":
     ip = "127.0.0.1"
     notation = "0"
     scan_mode = "none"
-    top = True
-    auto = False
-    sleeper = 3
-    dev = True
+    top = False
+    auto = True
+    sleeper = 0
+    dev = False
     scanner = scan()
     scanner.scan(ip, notation, top, auto)
 
