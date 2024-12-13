fcb2afa57b1564d6cb5130fc814cd8b4bc8b939e
SpaceWaffle42 <86594101+SpaceWaffle42@users.noreply.github.com>
2024-12-13 19:19:28 +0000

    Create datatables.js
    used for the overview table for easier use

---
+++ static/js/datatables.js
@@ -0,0 +1,27 @@
+function initializeDataTables() {
+
+    if (!$.fn.DataTable.isDataTable('#data-table')) {
+      $('#data-table').DataTable();
+    }
+  }
+
+  // detect table changes (more dynamic)
+  function observeTableUpdates() {
+    const config = { childList: true, subtree: true };
+    const observer = new MutationObserver((mutationsList) => {
+      for (let mutation of mutationsList) {
+        if (mutation.addedNodes.length > 0) {
+          initializeDataTables();
+          observer.disconnect(); // Stop observing once initialized
+        }
+      }
+    });
+    observer.observe(document.getElementById('data-table'), config);
+  }
+
+  $(document).ready(function () {
+    // Delay initialization for dynamically loaded tables
+    setTimeout(initializeDataTables, 1000);
+
+    observeTableUpdates();
+  });
