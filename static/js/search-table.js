714ef3d968f990a0a38e57f021de299c924c7304
SpaceWaffle42 <86594101+SpaceWaffle42@users.noreply.github.com>
2024-12-13 19:20:13 +0000

    Uses new data source

--- static/js/search-table.js
+++ static/js/search-table.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/data")
+    fetch("/summaryData")
         .then((response) => response.json())
         .then((data) => {
             const tableBody = document.querySelector("#search-table tbody");
@@ -9,17 +9,16 @@ document.addEventListener("DOMContentLoaded", function () {
             data.forEach((item) => {
                 const ip = Object.keys(item)[0];
                 const details = item[ip];
-
                 const row = document.createElement("tr");
                 row.innerHTML = `
                     <td>${details.date || "N/A"}</td>
                     <td>${ip || "N/A"}</td>
                     <td>${details.notation || "N/A"}</td>
-                    <td>${details.host_name || "N/A"}</td>
+                    <td>${details.total|| "N/A"}</td>
                 `;
 
                 tableBody.appendChild(row);
             });
         })
-        .catch((error) => console.error("Error fetching data:", error));
+    
 });
