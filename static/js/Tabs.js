97873183c8158bfeb7e254b5475b07580f77b76b
SpaceWaffle42 <86594101+SpaceWaffle42@users.noreply.github.com>
2024-12-31 17:24:01 +0000

    Mobile device check

--- static/js/Tabs.js
+++ static/js/Tabs.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const isMobile = window.innerWidth <= 768; //Mobile device width
+
   var swipeableTabs = document.querySelectorAll('.swipeable-tabs');
-  M.Tabs.init(swipeableTabs, { swipeable: true });
+  M.Tabs.init(swipeableTabs, { swipeable: !isMobile });
 
   var Tabs = document.querySelectorAll('.tabs:not(.swipeable-tabs)');
   M.Tabs.init(Tabs, { swipeable: false });
